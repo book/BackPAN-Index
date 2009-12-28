4799a500e1361c6a93c4cfa1ceda931183b6d455
Michael G. Schwern <schwern@pobox.com>
2009-12-28 01:21:06 -0800

    Remove unnecessary PBP instance subclasses.

--- lib/Parse/BACKPAN/Packages/File.pm
+++
@@ -1,8 +0,0 @@
-package Parse::BACKPAN::Packages::File;
-
-use strict;
-use warnings;
-
-use parent qw(BackPAN::Index::File);
-
-1;
--- lib/Parse/BACKPAN/Packages/Release.pm
+++
@@ -1,8 +0,0 @@
-package Parse::BACKPAN::Packages::Release;
-
-use strict;
-use warnings;
-
-use parent qw(BackPAN::Index::Release);
-
-1;
