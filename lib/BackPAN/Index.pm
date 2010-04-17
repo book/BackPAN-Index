552944f57af00f8e693222cbbc499889ccf65b94
Michael G. Schwern <schwern@pobox.com>
2010-04-17 20:23:03 +0300

    Version 0.39

--- lib/BackPAN/Index.pm
+++ lib/BackPAN/Index.pm
@@ -3,7 +3,7 @@ package BackPAN::Index;
 use strict;
 use warnings;
 
-our $VERSION = '0.38';
+our $VERSION = '0.39';
 
 use autodie;
 use App::Cache 0.37;
--- lib/Parse/BACKPAN/Packages.pm
+++ lib/Parse/BACKPAN/Packages.pm
@@ -3,7 +3,7 @@ package Parse::BACKPAN::Packages;
 use strict;
 use warnings;
 
-our $VERSION = '0.38';
+our $VERSION = '0.39';
 
 use parent qw(Class::Accessor::Fast);
 
