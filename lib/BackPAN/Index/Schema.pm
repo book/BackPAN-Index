c7dc074c092de831f42d6332450721bc064df65c
Michael G. Schwern <schwern@pobox.com>
2012-12-27 02:12:32 -0800

    Remove the DBIC::Schema::Loader cruft.
    This isn't generated by Schema::Loader.

--- lib/BackPAN/Index/Schema.pm
+++ lib/BackPAN/Index/Schema.pm
@@ -1,8 +1,4 @@
-use utf8;
 package BackPAN::Index::Schema;
-
-# Created by DBIx::Class::Schema::Loader
-# DO NOT MODIFY THE FIRST PART OF THIS FILE
 
 use strict;
 use warnings;
@@ -13,10 +9,4 @@ __PACKAGE__->load_classes({
     "BackPAN::Index" => [qw(Dist File Release)],
 });
 
-
-# Created by DBIx::Class::Schema::Loader v0.07033 @ 2012-12-27 01:39:08
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:nuF+3I0+Ir1lFmKHYH8kug
-
-
-# You can replace this text with custom code or comments, and it will be preserved on regeneration
 1;
