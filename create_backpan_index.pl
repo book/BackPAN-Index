bf5d5e7f50c2ec2c853019af9ae174e67f600443
Michael G. Schwern <schwern@pobox.com>
2014-08-14 23:24:24 -0700

    Update the create_backpan_index.pl program.
    This is what is used on the Gitpan server, plus some other fixups.

--- create_backpan_index.pl
+++ create_backpan_index.pl
@@ -1,17 +1,49 @@
 #!/usr/bin/perl
+
 use strict;
 use warnings;
+use v5.12;
+use autodie;
 use File::Find::Rule;
 use IO::Zlib;
+use Path::Tiny;
 
-my $out = IO::Zlib->new("/vhosts/www.astray.com/site/manatee/root/tmp/backpan.txt.gz", "wb9") || die $!;
+my $backpan = path(shift)->absolute;
+my $outfile = path(shift)->absolute;
 
-chdir "BACKPAN";
+# Build in a temp file to avoid serving a half built index while building
+my $tmpfile = Path::Tiny->tempfile;
+my $out = IO::Zlib->new($tmpfile.'', "wb9") || die $!;
+
+chdir $backpan;
 foreach my $filename (sort File::Find::Rule->new->file->in(".")) {
-  my($dev,$ino,$mode,$nlink,$uid,$gid,$rdev,$size,
-      $atime,$mtime,$ctime,$blksize,$blocks)
-    = stat($filename);
-  print $out "$filename $mtime $size\n";
+    my $stat = path($filename)->stat;
+    say $out join " ", $filename, $stat->mtime, $stat->size;
 }
-chdir "..";
 $out->close;
+
+# Put it in place.
+$tmpfile->move($outfile);
+
+# Make extra sure the web server can read it.
+$outfile->chmod(0644);
+
+
+=head1 NAME
+
+create_backpan_index.pl - Create the BackPAN index file
+
+=head1 SYNOPSIS
+
+    create_backpan_index.pl path/to/backpan path/to/index.gz
+
+=head1 DESCRIPTION
+
+Creates the index used by BackPAN::Index.  Intended to be used on a
+BackPAN server.
+
+Each line is space delimited information about a file.
+
+    filepath mtime size
+
+=cut
